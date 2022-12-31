6f90c7f41aa4fdd6788a74dabec51b25d3d780a3
oxen6297 <oxen6297@naver.com>
2022-12-31 17:16:02 +0900

    메인화면 광고 및 로딩 다이얼로그 구현

--- app/src/main/java/com/example/gyunggimoney/MainViewModel.kt
+++ app/src/main/java/com/example/gyunggimoney/MainViewModel.kt
@@ -2,11 +2,13 @@ package com.example.gyunggimoney
 
 import android.content.Context
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
@@ -19,7 +21,7 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
 
     val spinnerItem: MutableLiveData<String> = MutableLiveData()
 
-    fun getStore(hashMap: HashMap<String,Any>,context: Context,dialog: LoadingDialog) {
+    fun getStore(hashMap: HashMap<String, Any>, context: Context, dialog: LoadingDialog) {
         dialog.show()
         viewModelScope.launch {
             try {
@@ -27,8 +29,14 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
                 _storeResponse.value = response
             } catch (e: Exception) {
                 Log.d("getStoreError", e.toString())
-                Toast.makeText(context,"불러오기에 실패하였습니다. 잠시 후 다시 시도해주세요.",Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+                Toast.makeText(context, "가맹점 목록을 불러오지 못했습니다 잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
+    }
+
+    fun scrollTop(recyclerView: RecyclerView){
+        recyclerView.smoothScrollToPosition(0)
     }
 }
